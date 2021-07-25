8e6c8ef9aa5c457d2b2c64347d0fbb28e5bc7520
Felipe Yan <71793658+felipeyan@users.noreply.github.com>
2021-07-25 12:01:58 -0300

    Project files and main screen design

---
+++ app/src/main/java/com/felipeyan/minima/MainActivity.java
@@ -0,0 +1,63 @@
+package com.felipeyan.minima;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        recyclerView = findViewById(R.id.mainRV);
+    }
+
+    public void openMenu(View view) {
+        PopupMenu menu = new PopupMenu(this, view);
+        menu.getMenu().add(R.string.menu_settings);
+        menu.getMenu().add(R.string.menu_about);
+
+        menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getTitle().toString().equals(getString(R.string.menu_settings))) {
+                    openSettings();
+                    return true;
+                } else if (item.getTitle().toString().equals(getString(R.string.menu_about))) {
+                    openAbout();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+
+        menu.show();
+    }
+
+    public void addNote(View view) {
+        Toast.makeText(this, R.string.in_dev, Toast.LENGTH_SHORT).show();
+    }
+
+    public void openSettings() {
+        Toast.makeText(this, R.string.menu_settings, Toast.LENGTH_SHORT).show();
+    }
+
+    public void openAbout() {
+        Toast.makeText(this, R.string.menu_about, Toast.LENGTH_SHORT).show();
+    }
+}
